ff02017a310e2b10ed0f0bdae2c455f707985e51
Lewis <canlewi@s1029127ca.srn.sandia.gov>
2019-01-08 10:39:39 -0800

    Allgather for foolproof code

--- mpi/scramble_test/scramble.cc
+++ mpi/scramble_test/scramble.cc
@@ -20,8 +20,8 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 #define sstmac_app_name scramble_test
 int main(int argc, char* argv[]) {
@@ -57,26 +57,26 @@ int main(int argc, char* argv[]) {
   MPI_Barrier(MPI_COMM_WORLD);
   auto scram_host = print_hostnames("Scrambled Comm", scramble_comm);
 
+  std::vector<char> org_host(256 * world_size);
+  std::vector<char> new_host(256 * world_size);
   std::vector<int> new_rank(world_size);
+
+  MPI_Allgather(world_host.c_str(), 256, MPI_CHAR, org_host.data(), 256,
+                MPI_CHAR, MPI_COMM_WORLD);
+  MPI_Allgather(scram_host.c_str(), 256, MPI_CHAR, new_host.data(), 256,
+                MPI_CHAR, scramble_comm);
   MPI_Allgather(&me, 1, MPI_INT, new_rank.data(), 1, MPI_INT, MPI_COMM_WORLD);
 
   if (world_me == 0) {
-    std::vector<char> org_host(256 * world_size);
-    std::vector<char> new_host(256 * world_size);
-
-    MPI_Gather(world_host.c_str(), 256, MPI_CHAR, org_host.data(), 256, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Gather(scram_host.c_str(), 256, MPI_CHAR, new_host.data(), 256, MPI_CHAR, new_rank[0], scramble_comm);
-
-    std::cout << "(COMM_WORLD_RANK -> SCRAM_RANK), (COMM_WORLD_HOST -> SCRAM_HOST)\n";
-    for(auto i = 0; i < world_size; ++i){
+    std::cout
+        << "(COMM_WORLD_RANK -> SCRAM_RANK), (COMM_WORLD_HOST -> SCRAM_HOST)\n";
+    for (auto i = 0; i < world_size; ++i) {
       auto old_name = std::string(&org_host[256 * i]);
       auto new_name = std::string(&new_host[256 * i]);
-      
-      std::cout << "(" << i << " -> " << new_rank[i] << "), (" << old_name << " -> " << new_name << ")\n";
+
+      std::cout << "(" << i << " -> " << new_rank[i] << "), (" << old_name
+                << " -> " << new_name << ")\n";
     }
-  } else {
-    MPI_Gather(world_host.c_str(), 256, MPI_CHAR, nullptr, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Gather(scram_host.c_str(), 256, MPI_CHAR, nullptr, 256, MPI_CHAR, new_rank[0], scramble_comm);
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
